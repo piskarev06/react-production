d53c4e08eabc5048e07a8df631c1532997880d3c
Данила <a1111@MacBook-Pro-Danila.local>
2024-01-07 16:40:23 +0300

    add themes and ThemContext

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,23 @@
-import "../index.scss";
+import "../styles/index.scss";
 import { Route, Routes } from "react-router-dom";
 import { MainPageAsync } from "../pages/MainPage/MainPage.async";
 import { AboutPageAsync } from "../pages/AboutPage/AboutPage.async";
 import { Link } from "react-router-dom";
 import { Suspense } from "react";
+import { useTheme } from "../theme/useTheme";
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path="/about" element={<AboutPageAsync />} />
-          <Route path="/" element={<MainPageAsync />} />
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
         </Routes>
       </Suspense>
     </div>
