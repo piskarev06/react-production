c6d85dd14975773151952669fc863c3333fb57df
Danila <msabirova34@gmail.com>
2024-02-18 13:53:17 +0300

    add ErrorBoundary

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,9 +1,16 @@
+import { BugButton } from 'app/providers/ErrorBoundary';
 import { useTranslation } from 'react-i18next';
 
 const AboutPage = () => {
   const { t } = useTranslation();
 
-  return <div>{t('О сайте')}</div>;
+  return (
+    <div>
+      {t('О сайте')}
+
+      <BugButton />
+    </div>
+  );
 };
 
 export default AboutPage;
