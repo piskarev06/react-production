3c522cfe5bf59b198bb173ac80b1360c4fd4c5f4
Danila <msabirova34@gmail.com>
2024-02-18 11:55:12 +0300

    add NotFoundPage

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,24 +1,31 @@
-import { AboutPageAsync } from "pages/AboutPage/ui/AboutPage.async";
-import { MainPageAsync } from "pages/MainPage/ui/MainPage.async";
-import { RouteProps } from "react-router-dom";
+import { AboutPageAsync } from 'pages/AboutPage/ui/AboutPage.async';
+import { MainPageAsync } from 'pages/MainPage/ui/MainPage.async';
+import { NotFoundPage } from 'pages/NotFoundPage/ui/NotFoundPage';
+import { RouteProps } from 'react-router-dom';
 
 export enum AppRoutes {
-  MAIN = "main",
-  ABOUT = "about",
+  MAIN = 'main',
+  ABOUT = 'about',
+  NOT_FOUND = 'not_found'
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
-  [AppRoutes.MAIN]: "/",
-  [AppRoutes.ABOUT]: "/about",
+  [AppRoutes.MAIN]: '/',
+  [AppRoutes.ABOUT]: '/about',
+  [AppRoutes.NOT_FOUND]: '*'
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.MAIN]: {
     path: RoutePath.main,
-    element: <MainPageAsync />,
+    element: <MainPageAsync />
   },
   [AppRoutes.ABOUT]: {
     path: RoutePath.about,
-    element: <AboutPageAsync />,
+    element: <AboutPageAsync />
   },
+  [AppRoutes.NOT_FOUND]: {
+    path: RoutePath.not_found,
+    element: <NotFoundPage />
+  }
 };
